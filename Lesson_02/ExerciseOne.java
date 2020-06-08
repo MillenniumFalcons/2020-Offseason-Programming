b9abebea54e735b6b2c8770155ea3a27403305aa
suryamadhan <suryamadhan@gmail.com>
2020-06-08 14:32:33 -0700

    upload lesson 2 exercises

---
+++ Lesson_02/ExerciseOne.java
@@ -0,0 +1,29 @@
+/*
+Lesson _02
+In this exercise, we will use variables to print a sample ID card
+A sample ID Card will look like this:
+***************************************
+Surya Madhan         Millennium Falcons
+Programming                   Team 3647
+Proficient in Java            2019-2020
+***************************************
+Good Luck Completing the exercise!
+*/
+
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String name, teamName, department, somethingToBragAbout, schoolYear;
+        int teamNumber;
+        
+        System.out.println("***************************************");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("***************************************");
+      }
+    
+}
---
+++ Lesson_02/ExerciseThree.java
@@ -0,0 +1,22 @@
+/*
+Lesson _02
+In this exercise, you will use if, else if, and else statements to determine dedication of robotics members depending on build season hours
+For example:
+300+ hours: dedication level = "dedication on steroids"
+200+ hours: dedication level = "very dedicated"
+150+ hours: dedication level = "really dedicated"
+120+ hours: dedication level = "dedicated"
+90+ hours: dedication level = "average dedication"
+50+ hours: dedication level = "un-dedicated"
+else: dedication level = "just leave the team"
+Good Luck Completing the exercise!
+*/
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+         int buildSeasonHours;
+         String dedicationLevel;
+    }
+}
