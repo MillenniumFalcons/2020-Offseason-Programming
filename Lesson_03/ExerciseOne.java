35f775a1015f7eda6ce0b785ab5a5546e4b4f956
suryamadhan <suryamadhan@gmail.com>
2020-06-10 14:46:06 -0700

    upload exercise one

---
+++ Lesson_03/ExerciseOne.java
@@ -0,0 +1,18 @@
+/*
+Lesson _03
+In this exercise you will use while loops to create a refrigerator program
+You are going to use this program cool some food to a cretain temperature
+ */
+
+
+public class Main
+{
+    public static void main(String[]args)
+    {
+        double coolingSpeed = 10; //Temperature of object decreases 10 degrees per hours
+        double initialTempOfFood = 81;
+        double tempRequired = 51;
+        double time;//should be in hours
+    }
+
+}
