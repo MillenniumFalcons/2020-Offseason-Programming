ee58df57e9a394100c60a701e6718694978584da
suryamadhan <suryamadhan@gmail.com>
2020-06-14 14:59:23 -0700

    Upload Exercises

---
+++ Lesson_04/ExerciseOne.java
@@ -0,0 +1,32 @@
+/*
+Lesson _04
+In this exercise, you will use return functions to calculate the slope between 2 points and the distance between 2 points
+If the variable var is equal to 1 then calculate the distance between 2 points
+Else If variable var is equal to 2 then calculate the slope between 2 points
+Else var does not have an appropriate value.
+Good Luck Completing the exercise!
+*/
+
+
+public class Main
+{
+    public static void main(String[]args)
+    {
+        int var = 1;
+        double x1 = 1;
+        double y1 = 1;
+        double x2 = 5;
+        double y2 = 1;
+        
+    }
+    
+    static void calcDist()//make sure you enter parameters
+    {
+        
+    }
+    
+    static void calcSlope()//make sure you enter parameters
+    {
+        
+    }
+}
---
+++ Lesson_04/ExerciseTwo.java
@@ -0,0 +1,29 @@
+/*
+Lesson _04
+In this exercise, you will use void functions to calculate magnitude and the direction of a vector from their x-component, and y-component
+Feel free to look up the formulas for both finding the direction and the magnitude of a vector
+Remember that you don't need parameters for this exercise, because the variables are static
+Good Luck Completing the exercise!
+*/
+
+public class Main
+{
+    static double xComponent, yComponent, magnitude, direction;
+    
+    public static void main(String[]args)
+    {
+        xComponent = 6;
+        yComponent = 8;
+        
+    }
+    
+    public static void calcTheta() //use this function to calculate the direction of the vector
+    {
+        
+    }
+    
+    public static void calcMagnitude() //use this function to calculate the magnitude of the vector
+    {
+        
+    }
+}
