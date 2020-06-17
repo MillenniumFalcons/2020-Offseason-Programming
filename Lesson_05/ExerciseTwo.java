878676c9ebdd42edb2b5c8362c3eda13513fdbb1
suryamadhan <suryamadhan@gmail.com>
2020-06-17 15:00:32 -0700

    eejbwkfbejj

---
+++ Lesson_05/ExerciseTwo.java
@@ -0,0 +1,17 @@
+/*
+Lesson _03
+In this exercise, you will use nested for loops in order to print the following:
+ \:::::::/
+  \:::::/
+   \:::/
+    \:/
+Good Luck Completing the exercise!
+*/
+
+public class ExerciseTwo
+{
+    public static void main(String[]args)
+    {
+
+    }
+}
