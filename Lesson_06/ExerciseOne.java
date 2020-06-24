0ec61144a393e87f201f2e3de4a12bc814a3b141
suryamadhan <suryamadhan@gmail.com>
2020-06-24 14:59:17 -0700

    upload lesson_06 exercise

---
+++ Lesson_06/ExerciseOne.java
@@ -0,0 +1,28 @@
+/*
+Lesson _06
+In this exercise, you will make pizza!
+Order:
+    1. Order Ingredients
+    2. Put Ingredients together
+    3. Put Stuff In Oven
+    4. Take it out of Oven
+    5. EAT IT
+    6. DONE
+*/
+
+enum PIZZA {
+    ORDER_INGREDIENTS,
+    PUT_INGREDIENTS_TOGETHER,
+    PUT_PIZZA_IN_OVEN,
+    REMOVE_PIZZA_FROM_OVEN,
+    EAT_IT,
+    DONE
+}
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
