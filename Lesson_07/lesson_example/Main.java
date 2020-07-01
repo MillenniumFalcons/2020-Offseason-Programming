6ad6d360a7d6175f53d8363200f42abd3d728eba
haxoc <samsungs44@use.startmail.com>
2020-06-30 20:15:02 -0700

    Added exercise for lesson 7

---
+++ Lesson_07/lesson_example/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    Drivetrain dt;
+    public static void main(String[] args) {
+        runOnce();
+        while (true) {
+            runPeriodic();
+        }
+    }
+
+    static void runOnce() {
+
+    }
+
+    static void runPeriodic() {
+        dt.setMotors(Math.random() * 2 - 1, Math.random() * 2 - 1);
+        dt.printStatus();
+    }
+}
