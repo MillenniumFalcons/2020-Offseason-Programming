65e84b983ef71da688fa2244351cc2564d974309
suryamadhan <suryamadhan@gmail.com>
2020-07-06 14:51:52 -0700

    upload lesson 8 exercise

---
+++ Lesson_08/ExerciseOne.java
@@ -0,0 +1,52 @@
+/*
+Lesson _08
+In this exercise, you will use classes to create a simple calculator
+The operation you will be performing will depend on the run variable which will be an enum
+For this exercise please create all your classes in one file
+Good luck!
+ */
+
+public class Main
+{
+    public static void main(String[]args)
+    {
+        //make num object
+        
+        //variables used for calculations
+        double numOne = 34;
+        double numTwo = 67;
+        
+        //make objects
+        
+        //run conditional statements using the run variable
+    }
+
+}
+
+/*
+ Make a class called Addition
+ * Make necessary variables
+ * Make a constructor for that class
+ * Make a function called performOperation() that returns the sum of both the numbers
+ */
+
+/*
+Make a class called Subtraction
+* Make necessary variables
+* Make a constructor for that class
+* Make a function called performOperation() that returns the difference of both the numbers
+*/
+
+/*
+Make a class called Multiplication
+* Make necessary variables
+* Make a constructor for that class
+* Make a function called performOperation() that returns the product of both the numbers
+*/
+
+/*
+Make a class called Division
+* Make necessary variables
+* Make a constructor for that class
+* Make a function called performOperation() that returns the quotient of both the numbers
+*/
